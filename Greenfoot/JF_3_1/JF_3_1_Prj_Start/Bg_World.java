4cd5f7deee8c8b9da88d44bf7cec5949a4e25578
Made Andika Wiananda <114105365+KazamiKiro@users.noreply.github.com>
2022-12-30 21:15:20 +0800

    Add files via upload

---
+++ Greenfoot/JF_3_1/JF_3_1_Prj_Start/Bg_World.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Bg_World here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bg_World extends World
+{
+
+    /**
+     * Constructor for objects of class Bg_World.
+     * 
+     */
+    public Bg_World()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        orang orang = new orang();
+        addObject(orang,297,205);
+        block block = new block();
+        addObject(block,450,98);
+    }
+}
